ed8052833fda9cb9e4fd1bf39a63311042eecd9c
Jakob <jakob@avforlaget.dk>
2017-12-01 18:37:31 +0100

    Cleanup & more sensible function names

--- trnsd.js
+++ trnsd.js
@@ -1,14 +1,5 @@
 const map = f => r => (a, x) => r(a, f(x))
 , filter = f => r => (a, x) => f(x)? r(a, x): a
-/*
-, filter = f => r => (a, x)=> {
-     const b = f(x);
-    if (b instanceof Promise)
-      return b.then(b => b? r(a, x): a);
-    else
-      return b? r(a, x): a;
-  }
-*/
 , compose = (...fs) => fs.reverse().reduce((c, f) => f(c))
 , trnsd = (xs, a, r, ...fs) => xs.reduce(compose(...fs, r), a)
 
@@ -17,54 +8,37 @@ const map = f => r => (a, x) => r(a, f(x))
 
 , resolve = x => (x instanceof Promise? x: Promise.resolve(x))
 
-, r_intermediate = r => (pa, px) => resolve(px).then(x => r(pa, x))
-, r_async = r => (pa, px) => resolve(pa).then(a => resolve(px).then(x => r(a, x)))
+, tx_intermediate = r => (pa, px) => resolve(px).then(x => r(pa, x))
+, tx_async = r => (pa, px) => resolve(pa).then(a => resolve(px).then(x => r(a, x)))
+
 , interleave = (xs, y) => xs.slice(1).reduce((xyxs, x) => xyxs.concat(y, x), [xs[0]])
-/*
-, catching = (reducer, acc, x) => {
 
-
-acc.catch(e => console.log("WITCHES", e))
-
-return reducer(acc, x).catch(
-e => {
-  console.log("dammit", e);
-  //if (!error) {
-    //error = true;
-    throw e
-  //}
-}
-)
-
-}
-*/
 , trnsd_async = (xs, a, r, ...fs) => {
-  const reducer = compose(...interleave(fs, r_intermediate), r_async, r_array)
+  const reducer = compose(...interleave(fs, tx_intermediate), tx_async, r)
   let acc = a, too_late = false
 
-return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
 
-  const errorHandler = e => {
-    if (too_late) console.log("Swallowed: ", e.message)
-    else {
-      too_late = true;
-      reject(e)
+    const errorHandler = e => {
+      if (too_late) console.log("Swallowed: ", e.message)
+      else {
+        too_late = true;
+        reject(e)
+      }
     }
-  }
 
-  try {
-    for (let x of xs) {
-      // console.log(c++)
-      acc = reducer(acc, x).catch(errorHandler)//.catch(e => console.log("in", c, e)))
+    try {
+      for (let x of xs) {
+        acc = reducer(acc, x).catch(errorHandler)
+      }
+      acc.then(resolve)
+    } catch (e) {
+      errorHandler(e)
     }
-    acc.then(resolve)
-  } catch (e) {
-    errorHandler(e)
-  }
-})
+  })
 }
 , tr_async = (xs, ...fs) => trnsd_async(xs, [], r_array, ...fs)
 
 module.exports = {
-  map, filter, trnsd, tr_array, trnsd_async, tr_async,r_async,r_array
+  map, filter, trnsd, tr_array, trnsd_async, tr_async, tx_async, r_array
 }
